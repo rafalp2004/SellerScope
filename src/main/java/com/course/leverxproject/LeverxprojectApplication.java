2ce11beff67e9c3918e2268d70d1fcacf93939e3
rafalp2004 <rafalprzybyl17@gmail.com>
2025-02-28 14:31:12 +0100

    first commit

---
+++ src/main/java/com/course/leverxproject/LeverxprojectApplication.java
@@ -0,0 +1,13 @@
+package com.course.leverxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeverxprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeverxprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/leverxproject/LeverxprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.leverxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeverxprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
